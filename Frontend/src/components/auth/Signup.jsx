f2f5fe27b0390ebd36d436139881cbb89890818a
nileshottoedge <nilesh@ottoedge.com>
2024-07-30 14:32:46 +0530

    Login and signup all the logic is been done and toaster issue is been solved

--- Frontend/src/components/auth/Signup.jsx
+++ Frontend/src/components/auth/Signup.jsx
@@ -1,20 +1,81 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Navbar from '../shared/Navbar'
 import { Label } from '../ui/label'
 import { Input } from '../ui/input'
 import { RadioGroup } from '../ui/radio-group'
+import { Button } from '../ui/button'
+import { Link, useNavigate } from 'react-router-dom'
+import axios from 'axios'
+import { USER_API_END_POINT } from '@/utils/constant'
+import { toast } from 'sonner'
 
 const Signup = () => {
+
+
+    const [input, setInput] = useState({
+        fullname: "",
+        email: "",
+        password: "",
+        phoneNumber: "",
+        role: "",
+        file: ""
+    })
+
+    const navigate = useNavigate()
+
+
+    const changeEventHandler = (e) => {
+        setInput({ ...input, [e.target.name]: e.target.value });
+    }
+    const changeFileHandler = (e) => {
+        setInput({ ...input, file: e.target.files?.[0] });
+    }
+
+    const submitHandler = async (e) => {
+        e.preventDefault();
+        const formData = new FormData();
+        formData.append("fullname", input.fullname);
+        formData.append("email", input.email);
+        formData.append("phoneNumber", input.phoneNumber);
+        formData.append("password", input.password);
+        formData.append("role", input.role);
+        if (input.file) {
+            formData.append("file", input.file);
+        }
+        console.log('Form Data:', formData);
+        try {
+            const res = await axios.post(`${USER_API_END_POINT}/register`, formData, {
+                headers: { 'Content-Type': "multipart/form-data" },
+                withCredentials: true,
+            });
+    
+            if (res.data.success) {
+                navigate("/login");
+                toast.success(res.data.message);
+            }else {
+                toast.error('An unexpected error occurred.');
+            }
+        } catch (error) {
+            console.log('Error:', error);
+            if (error.response) {
+                console.log('Error Response:', error.response);
+                toast.error(error.response.data.message);
+            } 
+        }
+    };
     return (
         <div>
             <Navbar />
             <div className='flex items-center justify-center max-w-7xl mx-auto'>
-                <form action='' className='w-1/2 border border-gray-200 rounded-md p-4 my-10'>
+                <form onSubmit={submitHandler} className='w-1/2 border border-gray-200 rounded-md p-4 my-10'>
                     <h1 className='font-bold text-xl mb-5'>Sign Up</h1>
                     <div className='my-2'>
                         <Label>Full Name</Label>
                         <Input
                             type="text"
+                            value={input.fullname}
+                            name="fullname"
+                            onChange={changeEventHandler}
                             placeholder="Nilesh"
                         />
                     </div>
@@ -22,6 +83,9 @@ const Signup = () => {
                         <Label>Email</Label>
                         <Input
                             type="email"
+                            value={input.email}
+                            name="email"
+                            onChange={changeEventHandler}
                             placeholder="nilesh@gmail.com"
                         />
                     </div>
@@ -29,6 +93,9 @@ const Signup = () => {
                         <Label>Phone Number</Label>
                         <Input
                             type="text"
+                            value={input.phoneNumber}
+                            name="phoneNumber"
+                            onChange={changeEventHandler}
                             placeholder="9874563218"
                         />
                     </div>
@@ -36,6 +103,9 @@ const Signup = () => {
                         <Label>Password</Label>
                         <Input
                             type="password"
+                            value={input.password}
+                            name="password"
+                            onChange={changeEventHandler}
                             placeholder="Password"
                         />
                     </div>
@@ -46,6 +116,8 @@ const Signup = () => {
                                     type="radio"
                                     name="role"
                                     value="student"
+                                    checked={input.role === "student"}
+                                    onChange={changeEventHandler}
                                     className="cursor-pointer"
                                 />
                                 <Label htmlFor="r1">Student</Label>
@@ -55,6 +127,8 @@ const Signup = () => {
                                     type="radio"
                                     name="role"
                                     value="recruiter"
+                                    checked={input.role === "recruiter"}
+                                    onChange={changeEventHandler}
                                     className="cursor-pointer"
                                 />
                                 <Label htmlFor="r2">Recruiter</Label>
@@ -65,12 +139,14 @@ const Signup = () => {
                             <Input
                                 accept="image/*"
                                 type="file"
-
+                                onChange={changeFileHandler}
                                 className="cursor-pointer"
                             />
                         </div>
                     </div>
 
+                    <Button type="submit" className="w-full my-4">Signup</Button>
+                    <span className='text-sm'>Already have an account? <Link to="/login" className='text-blue-600'>Login</Link></span>
                 </form>
             </div>
         </div>
